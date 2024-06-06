ca8dc07452d198cf38605d9fdd21e65469b29bf9
extrymes <simon.abras@gmail.com>
2024-06-06 18:42:00 +0200

    feat: read map and get map data

--- so_long.c
+++ so_long.c
@@ -6,11 +6,23 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 09:02:00 by sabras            #+#    #+#             */
-/*   Updated: 2024/05/29 09:02:18 by sabras           ###   ########.fr       */
+/*   Updated: 2024/06/06 17:46:11 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "includes/so_long.h"
+#include "includes/libft.h"
+
 int	main(int ac, char **av)
 {
-	
+	char		**map;
+	t_map_data	map_data;
+
+	if (ac != 2)
+		return (ft_putstr("Error\nInvalid argument"), 1);
+	map = ft_read_map(av[1]);
+	if (!map)
+		return (1);
+	map_data = ft_get_map_data(map);
+	return (0);
 }
