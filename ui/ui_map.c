996082f52cd14178da63ddd5aa81bf996d4f057b
extrymes <simon.abras@gmail.com>
2024-06-16 19:52:48 +0200

    feat(ui): add `ft_put_image()` function

--- ui/ui_map.c
+++ ui/ui_map.c
@@ -6,11 +6,13 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 10:04:52 by sabras            #+#    #+#             */
-/*   Updated: 2024/06/16 15:16:54 by sabras           ###   ########.fr       */
+/*   Updated: 2024/06/16 19:52:09 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
+
+static void	ft_put_image(t_game_data game, char obj, int x, int y);
 
 void	ft_draw_map(char **map, t_game_data game)
 {
@@ -23,14 +25,28 @@ void	ft_draw_map(char **map, t_game_data game)
 		x = 0;
 		while (map[y][x])
 		{
-			if (map[y][x] == '0')
-				mlx_put_image_to_window(game.mlx_ptr, game.win_ptr,
-					game.space_img.ptr, x, y);
-			if (map[y][x] == '1')
-				mlx_put_image_to_window(game.mlx_ptr, game.win_ptr,
-					game.wall_img.ptr, x, y);
+			ft_put_image(game, map[y][x], x * SPRITE_SIZE, y * SPRITE_SIZE);
 			x++;
 		}
 		y++;
 	}
+}
+
+static void	ft_put_image(t_game_data game, char obj, int x, int y)
+{
+	if (obj == '0')
+		mlx_put_image_to_window(game.mlx_ptr, game.win_ptr,
+			game.space_img.ptr, x, y);
+	else if (obj == '1')
+		mlx_put_image_to_window(game.mlx_ptr, game.win_ptr,
+			game.wall_img.ptr, x, y);
+	else if (obj == 'C')
+		mlx_put_image_to_window(game.mlx_ptr, game.win_ptr,
+			game.collec_img.ptr, x, y);
+	else if (obj == 'P')
+		mlx_put_image_to_window(game.mlx_ptr, game.win_ptr,
+			game.player_img.ptr, x, y);
+	else if (obj == 'E')
+		mlx_put_image_to_window(game.mlx_ptr, game.win_ptr,
+			game.exit_img.ptr, x, y);
 }
